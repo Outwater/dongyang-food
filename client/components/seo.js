cdcd8d504866842c9fbd1e36cede1d7160105d20
Insoo-Seo <kocon135@gmail.com>
2022-11-22 12:10:10 +0900

    [feat] 페이지별 seo 적용을 위한 Seo 컴포넌트 추가

---
+++ client/components/seo.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import { useContext } from "react";
+import { getStrapiURL } from "../lib/request";
+import { GlobalContext } from "../pages/_app";
+
+const Seo = ({ seo }) => {
+  const { defaultSeo, SiteName } = useContext(GlobalContext);
+  const seoWithDefaults = {
+    ...defaultSeo,
+    ...seo,
+  };
+  const fullSeo = {
+    ...seoWithDefaults,
+    metaTitle: `${seoWithDefaults.metaTitle} | ${SiteName}`,
+    shareImage: getStrapiURL(seoWithDefaults.shareImage.data.attributes.url),
+  };
+
+  const { metaTitle, metaDescription, shareImage } = fullSeo;
+
+  return (
+    <Head>
+      {metaTitle && (
+        <>
+          <title>{metaTitle}</title>
+          <meta property="og:title" content={metaTitle} />
+        </>
+      )}
+      {metaDescription && (
+        <>
+          <meta name="description" content={metaDescription} />
+          <meta property="og:description" content={metaDescription} />
+        </>
+      )}
+      {shareImage && (
+        <>
+          <meta name="image" content={shareImage} />
+          <meta property="og:image" content={shareImage} />
+        </>
+      )}
+    </Head>
+  );
+};
+
+export default Seo;
