b7f5700d6a0f65a7dfa9393e393f9929f2b79f36
Insoo-Seo <kocon135@gmail.com>
2022-11-24 18:31:08 +0900

    [feat] Navbar에서 선택된 경로에 대하여 style 처리

--- client/components/nav.js
+++ client/components/nav.js
@@ -1,26 +1,32 @@
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
-const Nav = () => (
-  <NavContainer>
-    <Logo>동양 식자재마트(_logo)</Logo>
-    <LinkContainer>
-      <LeftSide>
-        <Link href="/" passHref legacyBehavior>
-          <StyledLink>상품 목록</StyledLink>
-        </Link>
-        <Link href="/price" passHref legacyBehavior>
-          <StyledLink>가격표 보기</StyledLink>
-        </Link>
-      </LeftSide>
-      <RightSide>
-        <Link href="/admin" passHref legacyBehavior>
-          <StyledLink>관리자(_hidden)</StyledLink>
-        </Link>
-      </RightSide>
-    </LinkContainer>
-  </NavContainer>
-);
+const Nav = () => {
+  const { pathname: currentPath } = useRouter();
+  const isActive = (path) => (path === "/" ? currentPath === path : currentPath.startsWith(path));
+
+  return (
+    <NavContainer>
+      <Logo>동양 식자재마트(_logo)</Logo>
+      <LinkContainer>
+        <LeftSide>
+          <Link href="/" passHref legacyBehavior>
+            <StyledLink active={isActive("/")}>상품 목록</StyledLink>
+          </Link>
+          <Link href="/price" passHref legacyBehavior>
+            <StyledLink active={isActive("/price")}>가격표 보기</StyledLink>
+          </Link>
+        </LeftSide>
+        <RightSide>
+          <Link href="/admin" passHref legacyBehavior>
+            <StyledLink active={isActive("/admin")}>관리자(_hidden)</StyledLink>
+          </Link>
+        </RightSide>
+      </LinkContainer>
+    </NavContainer>
+  );
+};
 
 export default Nav;
 
@@ -55,28 +61,36 @@ const LeftSide = styled.div({
 
 const RightSide = styled.div({});
 
-const StyledLink = styled.a({
-  display: "inline-block",
-  boxSizing: "border-box",
-  width: "auto",
-  margin: "0",
-  outline: "none",
-  padding: "13px 23px",
+const StyledLink = styled.a(
+  {
+    display: "inline-block",
+    boxSizing: "border-box",
+    width: "auto",
+    margin: "0",
+    outline: "none",
+    padding: "13px 23px",
 
-  backgroundColor: "#ffffff",
-  border: "1px solid #222222",
-  borderRadius: "8px",
+    backgroundColor: "#ffffff",
+    border: "1px solid #222222",
+    borderRadius: "8px",
 
-  color: "#222222",
-  fontSize: "16px",
-  fontWeight: "600",
-  lineHeight: "20px",
+    color: "#222222",
+    fontSize: "16px",
+    fontWeight: "600",
+    lineHeight: "20px",
 
-  position: "relative",
-  textAlign: "center",
-  textDecoration: "none",
+    position: "relative",
+    textAlign: "center",
+    textDecoration: "none",
 
-  touchAction: "manipulation",
-  transition: "box-shadow 0.2s, -ms-transform 0.1s, -webkit-transform 0.1s, transform 0.1s",
-  cursor: "pointer",
-});
+    touchAction: "manipulation",
+    transition: "box-shadow 0.2s, -ms-transform 0.1s, -webkit-transform 0.1s, transform 0.1s",
+    cursor: "pointer",
+    "&:hover": {
+      color: "#1dc078",
+    },
+  },
+  (props) => ({
+    color: props.active ? "#1dc078" : "#222222",
+  })
+);
