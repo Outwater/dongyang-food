badc2c593036358829d180b80193e502c4faa3a8
Insoo-Seo <kocon135@gmail.com>
2022-11-29 17:19:42 +0900

    [feat] global 스타일 적용 및 fontawesome 패키지 추가

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    emotion: true,
+  },
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
+  },
+  images: {
+    loader: "default",
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-mart.baemin.com",
+      },
+    ],
   },
 };
 
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,12 @@ import App from "next/app";
 import Head from "next/head";
 import { createContext } from "react";
 import request, { getStrapiURL } from "../lib/request";
-
+import { Global } from "@emotion/react";
+import reset from "client/style/reset";
+import globalStyle from "client/style/global";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 /*
  strapi로 부터 받아온 Global object를 저장한다.
 */
@@ -10,7 +15,6 @@ export const GlobalContext = createContext({});
 
 const MyApp = ({ Component, pageProps }) => {
   const { global } = pageProps;
-
   return (
     <>
       <Head>
@@ -19,6 +23,8 @@ const MyApp = ({ Component, pageProps }) => {
           href={getStrapiURL(global.attributes.favicon.data.attributes.url)}
         />
       </Head>
+      <Global styles={reset} />
+      <Global styles={globalStyle} />
       <GlobalContext.Provider value={global.attributes}>
         <Component {...pageProps} />
       </GlobalContext.Provider>
