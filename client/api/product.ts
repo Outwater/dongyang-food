b149f844497799473275f22369a477499744f9e2
Insoo-Seo <kocon135@gmail.com>
2022-12-21 23:38:10 +0900

    [fix] api option parameter nullable 하도록 수정

--- client/api/product.ts
+++ client/api/product.ts
@@ -1,12 +1,12 @@
 import request from "./utils/request";
 import { decodeProductList } from "./utils/decode";
 
-export const getProductList = async (urlParamsOptions = { populate: "*" }, options) => {
+export const getProductList = async (urlParamsOptions: any = { populate: "*" }, options?: any) => {
   const response = await request("/products", urlParamsOptions, options).then(decodeProductList);
   return response;
 };
 
-export const getCategoryList = async (urlParamsOptions = { populate: "*" }, options) => {
+export const getCategoryList = async (urlParamsOptions: any = { populate: "*" }, options?: any) => {
   const response = await request("/categories", urlParamsOptions, options);
   return response.data;
 };
